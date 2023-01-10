c6876cdba94b62e24644f93f989b83a73842b0c3
marked9999 <jewelsytofficial@gmail.com>
2023-01-10 12:52:02 +0000

    terminal made

--- programs/terminal.py
+++ programs/terminal.py
@@ -12,13 +12,19 @@ if loginCreds == AccessKey:
         if createFile == 'Y':
             fileCreateName = input('File Name?')
             f = open(researchFiles + fileCreateName + '.txt', 'x')
-            f = open(researchFiles + fileCreateName + '.txt')
+            f = open(researchFiles + fileCreateName + '.txt', 'a')
             toAdd = input('What to add?')
-            f.append('\n' + toAdd)
+            seconds = time.time()
+            local_time = time.ctime(seconds)
+
+            
+            f.write('\n' + local_time + '\n' + toAdd)
 
 
     elif menu1 == 'D':
         print('Connecting...')
         fileDeleteName = input('File name?')
         f = open(researchFiles + fileDeleteName + '.txt')
-        lineCount = len(open(f).readlines(  ))        
+        f.delete()
+        print('Deleted')
+            
